195776e884aec16f25b7c3d51a748fcaec496eb3
SanchitPandey26 <sanchitpandey263@gmail.com>
2024-05-24 11:05:15 +0530

    Uploading Patterns

---
+++ Patterns/src/Pattern7.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Pattern7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++){
+            for (int k = n-i; k > 0; k--){
+                System.out.print(' ');
+            }
+            for (int j = 0; j < 2*i-1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
