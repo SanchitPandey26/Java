222fae26c1be4aaa9250c8cc782ebefca1d427eb
SanchitPandey26 <sanchitpandey263@gmail.com>
2024-06-06 13:55:11 +0530

    Solution

---
+++ Sorting/src/Q5.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Q5 {
+    static int partition(int[] arr, int low, int high){
+        int pivot = arr[low];
+        int i = low;
+        int j = high;
+        while (i < j) {
+            while (arr[i] <= pivot && i < high) i++;
+            while (arr[j] > pivot && j > low) j--;
+            if (i < j) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[low];
+        arr[low] = arr[j];
+        arr[j] = temp;
+        return j;
+    }
+    static void quicksort(int[] arr, int low, int high){
+        if (low < high) {
+            int pIndex = partition(arr, low, high);
+            quicksort(arr, low, pIndex-1);
+            quicksort(arr, pIndex + 1, high);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        quicksort(arr,0,n-1);
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
